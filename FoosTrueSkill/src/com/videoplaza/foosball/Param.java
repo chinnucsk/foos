496cedaa197894c37cb669af4b4756f80f708fb0
joshua <josh@videoplaza.com>
2012-10-05 14:45:40 +0200

    Param : add LEADERBOARD_START_DATE

--- FoosTrueSkill/src/com/videoplaza/foosball/Param.java
+++ FoosTrueSkill/src/com/videoplaza/foosball/Param.java
@@ -3,5 +3,6 @@ package com.videoplaza.foosball;
 public class Param {
    public static final String START_DATE = "startDate";
    public static final String END_DATE = "endDate";
+   public static final String LEADERBOARD_START_DATE = "leaderboardStartDate";
    public static final String MIN_REQ_GAMES = "minReqGames";
 }
