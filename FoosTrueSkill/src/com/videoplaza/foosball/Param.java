935fc1054e5fb489b5071871a2822a19ecfc9b91
joshua <josh@videoplaza.com>
2012-12-07 15:50:48 +0100

    Param : add PLAYER

--- FoosTrueSkill/src/com/videoplaza/foosball/Param.java
+++ FoosTrueSkill/src/com/videoplaza/foosball/Param.java
@@ -5,4 +5,5 @@ public class Param {
    public static final String END_DATE = "endDate";
    public static final String LEADERBOARD_START_DATE = "leaderboardStartDate";
    public static final String MIN_REQ_GAMES = "minReqGames";
+   public static final String PLAYERS = "player";
 }
